3d4f58d066664c2d81e03d26335957b9d8d24549
drogon <abhiramajithr@gmail.com>
2024-07-03 10:25:10 +0530

    basic-langchain

--- main.py
+++
@@ -1,8 +0,0 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
-import os
-
-dotenv_path='.env'
-load_dotenv(dotenv_path)
-
-model = ChatGoogleGenerativeAI(model='gemini-1.5-flash')
