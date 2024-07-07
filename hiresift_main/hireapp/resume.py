26a3d7b2b9b3b872ae60a88b386f19a8c87abf5c
Abhiram A R <abhiramajithr@gmail.com>
2024-07-08 02:06:52 +0530

    vecto-embeddings-set

--- hiresift_main/hireapp/resume.py
+++ hiresift_main/hireapp/resume.py
@@ -2,26 +2,63 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.documents import Document
 from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_community import BigQueryVectorStore
 from dotenv import load_dotenv
+import os
 
-load_dotenv(dotenv_path='.env')
+load_dotenv(dotenv_path=".env")
 
 
-llm=ChatGoogleGenerativeAI(model='gemini-1.5-flash')
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/text-embedding-004",project_id="bold-site-427218-v5"
+)
+
+store = BigQueryVectorStore(
+    project_id="bold-site-427218-v5",
+    dataset_name='my_langchain_dataset',
+    table_name='doc_and_vectors',
+    location='us-central1',
+    embedding=embeddings,
+)
 
 def document_loader(file_path):
-    loader=PyPDFLoader(file_path)
-    resume=loader.load()
-    content=resume[0].page_content[0:]
+    loader = PyPDFLoader(file_path)
+    resume = loader.load()
+    content = resume[0].page_content[0:]
     return content
 
+
 def scanning(file_path):
-    prompt=PromptTemplate.from_template("{resume} is a resume of an applicant.{user_prompt}")
-    agent=prompt | llm
-    result=agent.invoke({"resume":document_loader(file_path),"user_prompt":"check whether this applicant show any skills in machine learning and ai"}).content
-    return result
+    prompt_template = PromptTemplate.from_template(
+        "{resume} is a resume of an applicant.{user_prompt}"
+    )
+    agent = prompt_template | llm
+    prompt={
+            "resume": document_loader(file_path),
+            "user_prompt": "check whether this applicant show any skills to be a software developer",
+        }
+    result = agent.invoke(prompt).content
+    return {
+        "result":result,
+        "prompt":f"{prompt.get('resume')} is a resume of an applicant.{prompt.get('user_prompt')}"
+    }
 
+def vector(id,content):
+    return Document(
+        page_content=content,
+        # metadata={"id":id}
+    )
 
-
-
-
+def vector_search(document,file_path):
+    store.add_documents(
+        document
+    )
+    load_query=scanning(file_path)
+    user_quer_text=load_query['prompt']
+    # query_vector=embeddings.embed_query(user_quer_text)
+    #print(user_quer_text)
+    docs= store.similarity_search(user_quer_text)
+    return docs[0].metadata
--- hiresift_main/hireapp/views.py
+++ hiresift_main/hireapp/views.py
@@ -1,32 +1,54 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import ApplicantData
-from .resume import document_loader,scanning
+from .resume import document_loader, scanning, vector, vector_search
 from .forms import ApplicantForm
 
+
 def main(request):
+    result=searching()
+    return render(request, "result.html", {"result": result})
+
+def vector_storing():
     content=documentation()
-    file_path=content.get('file_path')
-    result=scanning(file_path)
-    return render(request,'result.html',{'result':result})
+    id=content.get('id')
+    result=content.get('result')
+    document=[]
+    document.append(vector(id,result))
+    return {
+        "document":document,
+        "id":id,
+        "file_path":content.get('file_path')
+    }
+
+def searching():
+    loader=vector_storing()
+    document=loader['document']
+    id=loader['id']
+    file_path=loader['file_path']
+    search_results=vector_search(document,file_path)
+    return search_results
 
 def formSubmition(request):
-    if request.method == 'POST':
-        form = ApplicantForm(request.POST,request.FILES)
+    if request.method == "POST":
+        form = ApplicantForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('success')
-            
+            return redirect("success")
+
     else:
         form = ApplicantForm()
 
-    return render(request, 'upload_resume.html', {'form': form})
-    
+    return render(request, "upload_resume.html", {"form": form})
+
+
 def documentation():
-    applicants=ApplicantData.objects.all()
-    content={}
+    applicants = ApplicantData.objects.all()
+    content = {}
     for applicant in applicants:
-        file_name=applicant.get_file_name()
-        file_path=f"C:/Users/aaran/OneDrive/Desktop/HireSift-backend/hiresift_main/media/{file_name}"
-        content.update({"id":applicant.application_number,"file_path":file_path})
+        file_name = applicant.get_file_name()
+        file_path = f"C:/Users/aaran/OneDrive/Desktop/HireSift-backend/hiresift_main/media/{file_name}"
+        loader=scanning(file_path)
+        content.update({"id": applicant.application_number, "file_path": file_path,'result':loader["result"]})
     return content
+
